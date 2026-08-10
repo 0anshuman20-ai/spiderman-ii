3f99cdbadacdd89a3d772cfaf9fb52c1e98202ae
0anshuman20-ai <0anshuman2.0@gmail.com>
2026-08-10 15:06:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
-import "@/App.css";
+mport "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Studio from "@/pages/Studio";
+import OmegaRoom from "@/pages/OmegaRoom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Studio />} />
+        <Route path="/omega" element={<OmegaRoom />} />
       </Routes>
     </BrowserRouter>
   );
-}
-
-export default App;
